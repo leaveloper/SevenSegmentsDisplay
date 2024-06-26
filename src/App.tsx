ea2e2ae53290684efa41e018b918f98b85ff52ed
ljuarezz <leaveloper@gmail.com>
2024-06-26 13:51:36 -0300

    Corrección de estilos y botón 'Counter'

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import { Display } from './components/Display'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [number, setNumber] = useState(0);
+
+  const handleClick = () => {
+    if (number >= 9) {
+      setNumber(0);
+    } else {
+      setNumber(number + 1);
+    }
+  }
 
   return (
-    <div id="display">
-      <Display number={8}/>
+    <div id="display-container">
+      <div id="display">
+        <Display number={number}/>
+      </div>
+      <div id="button-container">
+        <button onClick={handleClick}>Count</button>
+      </div>
     </div>
   )
 }
